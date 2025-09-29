594aa1ab635f35c736180e23ccb04aa358cc2eae
jangabaev <jangabaevmuxtar12@gmail.com>
2025-09-29 23:32:34 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // import { Advertising } from "./components/advertising"
-import { Dashboard } from "./components/dashboard"
+// import { Dashboard } from "./components/dashboard"
 import { Navbar } from "./components/navbar"
-import { Process } from "./components/process"
-import { Footer } from "./components/footer"
+// import { Process } from "./components/process"
+// import { Footer } from "./components/footer"
 import { Contact } from "./components/contact"
 import { ComplexMarketing } from "./components/complexInflunse"
 import { ClientThink } from "./components/thinking"
@@ -11,13 +11,13 @@ function App() {
   return (
     <main className="main">
       <Navbar />
-      <Dashboard />
-      <Process />
+      {/* <Dashboard /> */}
+      {/* <Process /> */}
       {/* <Advertising/> */}
       <ComplexMarketing />
       <ClientThink />
       <Contact />
-      <Footer />
+      {/* <Footer /> */}
     </main>
   )
 }
